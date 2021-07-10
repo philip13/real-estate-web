13b91489463d1a449c60e667e8571ab8a48b1575
Felipe Cuevas <felipe@softwareallies.com>
2021-07-10 00:33:48 -0500

    src/app/shared/models/real-estate.* new model

---
+++ src/app/shared/models/real-estate.spec.ts
@@ -0,0 +1,7 @@
+import { RealEstate } from './real-estate';
+
+describe('RealEstates', () => {
+  it('should create an instance', () => {
+    expect(new RealEstate()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/real-estate.ts
@@ -0,0 +1,16 @@
+export class RealEstate {
+  id?: number;
+  name?: string;
+  real_state_type?: string;
+  street?: string;
+  external_number?: string;
+  internal_number?: string;
+  neighborhood?: string;
+  city?: string;
+  country?: string;
+  rooms?: number;
+  bathrooms?: number;
+  comments?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
