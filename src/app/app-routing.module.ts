c6cd868f8d59a897f3b2b9a020f35a4cb4c74d9c
Felipe Cuevas <felipe@softwareallies.com>
2021-07-10 00:36:22 -0500

    src/app/app-routing.module.ts new routes for display details and show list of RealEstates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { ShowComponent } from './show/show.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: IndexComponent },
+  { path: 'real-estates',  component: IndexComponent },
+  { path: 'real-estates/:id', component: ShowComponent },
 ];
 
 @NgModule({
